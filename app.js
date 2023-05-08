e94736dc01ff37105873ef0728a10871671c9b30
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2023-05-08 20:30:11 +0000

    Testing fetching more

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var P = new Pokedex.Pokedex({protocol: 'https'});
-
-P.getPokemonByName("pikachu") // with Promise
-.then(function(response) {
-  console.log(response);
-})
-.catch(function(err) {
-  console.log(err)
-});
+/*
+ * Need to get the pokemon information based on the text input  
+ * then need to parse the JSON and map it to the existing div elements
+ * use the fetch api and call based on id or name
+ * Figma to see layout: https://www.figma.com/file/gycXxCdV1kZ8yTCBt4xrc9/Pokedex-Prototype?type=whiteboard&node-id=0-1&t=zOB2OpLnAfoqohIP-0
+ */
+const fetchPokemon = () => {
+const url = `https://pokeapi.co/api/v2/pokemon/1`;
+}
